f121fd20a003685cfae14793c6ae70334fb57a68
jrstnly <jr@jrstn.ly>
2020-09-27 21:20:53 -0500

    Updated LED control

--- index.js
+++ index.js
@@ -2,29 +2,51 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const display = require('./display');
-const ledDriver = require('./led');
+const LedDriver = require('./led');
 
-app.get('/', function(req, res){
-	res.send('<h1>Office Controller Server</h1>');
-});
-http.listen(3000, function(){
-	console.log('Listening on *:3000');
-});
+(async () => {
+	const settings = {
+		led: [
+			{red: 0, green: 0, blue: 0},
+			{red: 0, green: 0, blue: 0},
+			{red: 0, green: 0, blue: 0},
+			{red: 0, green: 0, blue: 0}
+		]
+	}
+	const led = await new LedDriver;
+	const updateLED = () => {
+		settings.led.forEach((item, i) => {
+			led.set(i, item.red, item.green, item.blue);
+		});
+	}
 
-io.on('connection', function(clientSocket){
-	console.log('a user connected');
-
-	clientSocket.on('disconnect', function(){
-		console.log('user disconnected');
+	app.get('/', function(req, res){
+		res.send('<h1>Office Controller Server</h1>');
 	});
-});
+	http.listen(3000, function(){
+		console.log('Listening on *:3000');
+	});
 
-const calibrate = async () => {
-	await display.calibrateTop();
-	await display.calibrateBottom();
-	console.log("Calibration complete");
-};
+	io.on('connection', function(socket){
+		console.log('a user connected');
+		socket.emit('update', settings);
 
-const pwmDriver = ledDriver();
+		socket.on('control led', (data) => {
+			settings.led[data.channel] = data.color;
+			socket.broadcast.emit('update', settings);
+			updateLED();
+		})
 
-//calibrate();
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+		});
+	});
+
+	const calibrate = async () => {
+		await display.calibrateTop();
+		await display.calibrateBottom();
+		console.log("Calibration complete");
+	};
+
+	//calibrate();
+})();
--- led.js
+++ led.js
@@ -1,31 +1,39 @@
 const i2cBus = require('i2c-bus');
 const Pca9685Driver = require("pca9685").Pca9685Driver;
 
-const ledDriver = () => {
-	const options = { i2c: i2cBus.openSync(1), address: 0x40, frequency: 500, debug: true };
+class LedDriver {
+	pwm = null;
+	options = { i2c: i2cBus.openSync(1), address: 0x40, frequency: 800, debug: false };
 
-	const init = async () => {
-		new Promise((resolve) => {
-			const pwm = new Pca9685Driver(options, (err) => {
+	constructor() {
+		return new Promise((resolve) => {
+			this.pwm = new Pca9685Driver(this.options, (err) => {
 				if (err) {
 					console.error("Error initializing PCA9685");
 					process.exit(-1);
 				}
 				console.log("Initialization done");
-				resolve(pwm);
-			});
-		}).then((pwm) => {
-			pwm.setPulseRange(0, 0, 4095, (err) => {
-				if (err) {
-					console.error("Error setting pulse range.");
-				} else {
-					console.log("Pulse range set.");
-				}
+				resolve(this);
 			});
 		});
 	}
 
-	init();
+	set(channel, red, green, blue) {
+		const start = channel * 3;
+		const redVal = parseInt(((red / 100) * 4095).toFixed(0));
+		const greenVal = parseInt(((green / 100) * 4095).toFixed(0));
+		const blueVal = parseInt(((blue / 100) * 4095).toFixed(0));
+
+		this.pwm.setPulseRange(start, 0, redVal, (err) => {
+				if (err) console.error("Error setting pulse range.");
+		});
+		this.pwm.setPulseRange(start+1, 0, greenVal, (err) => {
+				if (err) console.error("Error setting pulse range.");
+		});
+		this.pwm.setPulseRange(start+2, 0, blueVal, (err) => {
+				if (err) console.error("Error setting pulse range.");
+		});
+	}
 }
 
-module.exports = ledDriver
+module.exports = LedDriver;
