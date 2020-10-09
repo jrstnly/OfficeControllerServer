89955c68ed6adc96fe36272a3a6f32c355e17935
jrstnly <jr@jrstn.ly>
2020-10-09 10:38:26 -0500

    Switched display control to a class

--- display.js
+++ display.js
@@ -1,110 +1,132 @@
+const { Accelerometer, Board } = require("johnny-five");
 const gpio = require('pigpio').Gpio;
+const Raspi = require('raspi-io').RaspiIO;
+const board = new Board({
+	io: new Raspi()
+});
 
 const MICROSECDONDS_PER_CM = 1e6/34321;
-let common = {}
 
 const displayUpGpio = new gpio(20, {mode: gpio.OUTPUT});
 const displayDownGpio = new gpio(21, {mode: gpio.OUTPUT});
+const displayTiltUpGpio = new gpio(6, {mode: gpio.OUTPUT});
+const displayTiltDownGpio = new gpio(13, {mode: gpio.OUTPUT});
 const displaySensorTrigger = new gpio(22, {mode: gpio.OUTPUT});
 const displaySensorEcho = new gpio(23, {mode: gpio.INPUT, alert: true});
 
-let displayMaxHeight = 0;
-let displayMinHeight = 0;
-let displayHeight = 0;
-let displayHeightInterval;
+class DisplayController {
+	displayMaxHeight = 0;
+	displayMinHeight = 0;
+	displayHeight = 0;
+	displayHeightInterval;
 
-const initDisplayControl = () => {
-	displayUpGpio.digitalWrite(0);
-	displayDownGpio.digitalWrite(0);
-	displaySensorTrigger.digitalWrite(0);
+	constructor() {
+		return new Promise((resolve) => {
+			displayUpGpio.digitalWrite(0);
+			displayDownGpio.digitalWrite(0);
+			displayTiltUpGpio.digitalWrite(0);
+			displayTiltDownGpio.digitalWrite(0);
+			displaySensorTrigger.digitalWrite(0);
 
-	let startTick;
-	displaySensorEcho.on('alert', (level, tick) => {
-		if (level == 1) {
-			startTick = tick;
-		} else {
-			const endTick = tick;
-			const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-			const distanceInCM = Math.floor(parseFloat(diff / 2 / MICROSECDONDS_PER_CM));
-			if (Math.abs(distanceInCM - displayHeight) < 10 || displayHeight === 0) displayHeight = distanceInCM;
-			console.log(displayHeight);
-		}
-	});
-};
-const startMeasuringHeight = () => {
-	displaySensorTrigger.trigger(10, 1); // Set trigger high for 10 microseconds
-	displayHeightInterval = setInterval(() => {
-		displaySensorTrigger.trigger(10, 1); // Set trigger high for 10 microseconds
-	}, 750);
-};
-const stopMeasuringHeight = () => {
-	clearInterval(displayHeightInterval);
-};
 
-common.stop = () => {
-	displayUpGpio.digitalWrite(0);
-	displayDownGpio.digitalWrite(0);
-	stopMeasuringHeight();
-};
-common.move = (direction) => {
-	startMeasuringHeight();
-	if (direction === 'up') {
-		displayUpGpio.digitalWrite(1);
-		displayDownGpio.digitalWrite(0);
+			let startTick;
+			displaySensorEcho.on('alert', (level, tick) => {
+				if (level == 1) {
+					startTick = tick;
+				} else {
+					const endTick = tick;
+					const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+					const distanceInCM = Math.floor(parseFloat(diff / 2 / MICROSECDONDS_PER_CM));
+					if (Math.abs(distanceInCM - this.displayHeight) < 10 || this.displayHeight === 0) this.displayHeight = distanceInCM;
+					console.log(this.displayHeight);
+				}
+			});
+		});
 	}
-	if (direction === 'down') {
+
+	stop() {
 		displayUpGpio.digitalWrite(0);
-		displayDownGpio.digitalWrite(1);
-	}
-};
+		displayDownGpio.digitalWrite(0);
+		this.stopMeasuringHeight();
+	};
+	move(direction) {
+		this.startMeasuringHeight();
+		if (direction === 'up') {
+			displayUpGpio.digitalWrite(1);
+			displayDownGpio.digitalWrite(0);
+		}
+		if (direction === 'down') {
+			displayUpGpio.digitalWrite(0);
+			displayDownGpio.digitalWrite(1);
+		}
+	};
+	tilt(direction) {
+		if (direction === 'up') {
+			displayTiltDownGpio.digitalWrite(0);
+			displayTiltUpGpio.digitalWrite(1);
+		}
+		if (direction === 'down') {
+			displayTiltUpGpio.digitalWrite(0);
+			displayTiltDownGpio.digitalWrite(1);
+		}
+	};
 
-common.calibrateTop = () => {
-	return new Promise((resolve) => {
-		let limit = 0;
-		let limitCount = 0;
-		common.move('up');
-		let counter = setInterval(() => {
-			if (limit === displayHeight) {
-				if (limitCount >= 4) {
-					console.log("Top limit reached");
-					displayMaxHeight = displayHeight - 2;
-					clearInterval(counter);
-					common.stop();
-					resolve();
+	calibrateTop() {
+		return new Promise((resolve) => {
+			let limit = 0;
+			let limitCount = 0;
+			this.move('up');
+			let counter = setInterval(() => {
+				if (limit === this.displayHeight) {
+					if (limitCount >= 4) {
+						console.log("Top limit reached");
+						this.displayMaxHeight = this.displayHeight - 2;
+						clearInterval(counter);
+						this.stop();
+						resolve();
+					} else {
+						limitCount++;
+					}
 				} else {
-					limitCount++;
+					limit = this.displayHeight;
+					limitCount = 0;
 				}
-			} else {
-				limit = displayHeight;
-				limitCount = 0;
-			}
-		}, 500);
-	});
-};
-common.calibrateBottom = () => {
-	return new Promise((resolve) => {
-		let limit = 0;
-		let limitCount = 0;
-		common.move('down');
-		let counter = setInterval(() => {
-			if (limit === displayHeight) {
-				if (limitCount >= 4) {
-					console.log("Bottom limit reached");
-					displayMinHeight = displayHeight + 2;
-					clearInterval(counter);
-					common.stop();
-					resolve();
+			}, 500);
+		});
+	};
+	calibrateBottom() {
+		return new Promise((resolve) => {
+			let limit = 0;
+			let limitCount = 0;
+			this.move('down');
+			let counter = setInterval(() => {
+				if (limit === this.displayHeight) {
+					if (limitCount >= 4) {
+						console.log("Bottom limit reached");
+						this.displayMinHeight = this.displayHeight + 2;
+						clearInterval(counter);
+						this.stop();
+						resolve();
+					} else {
+						limitCount++;
+					}
 				} else {
-					limitCount++;
+					limit = this.displayHeight;
+					limitCount = 0;
 				}
-			} else {
-				limit = displayHeight;
-				limitCount = 0;
-			}
-		}, 500);
-	});
-};
+			}, 500);
+		});
+	};
 
-initDisplayControl();
+	startMeasuringHeight() {
+		displaySensorTrigger.trigger(10, 1); // Set trigger high for 10 microseconds
+		this.displayHeightInterval = setInterval(() => {
+			displaySensorTrigger.trigger(10, 1); // Set trigger high for 10 microseconds
+		}, 750);
+	};
+	stopMeasuringHeight() {
+		clearInterval(this.displayHeightInterval);
+	};
+}
 
-module.exports = common;
+module.exports = DisplayController;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const display = require('./display');
+const DisplayController = require('./display');
 const LedDriver = require('./led');
 
 (async () => {
@@ -13,7 +13,16 @@ const LedDriver = require('./led');
 			{red: 0, green: 0, blue: 0}
 		]
 	}
+	const display = await new DisplayController;
 	const led = await new LedDriver;
+
+	const calibrate = () => {
+		return new Promise(async (resolve) => {
+			await display.calibrateTop();
+			await display.calibrateBottom();
+			resolve();
+		});
+	};
 	const updateLED = () => {
 		settings.led.forEach((item, i) => {
 			led.set(i, item.red, item.green, item.blue);
@@ -31,22 +40,20 @@ const LedDriver = require('./led');
 		console.log('a user connected');
 		socket.emit('update', settings);
 
+		socket.on('calibrate', async (data) => {
+			socket.broadcast.emit('message', 'Calibrating...');
+			await calibrate();
+			socket.broadcast.emit('message', 'Calibration complete.');
+		});
+
 		socket.on('control led', (data) => {
 			settings.led[data.channel] = data.color;
 			socket.broadcast.emit('update', settings);
 			updateLED();
-		})
+		});
 
 		socket.on('disconnect', () => {
 			console.log('user disconnected');
 		});
 	});
-
-	const calibrate = async () => {
-		await display.calibrateTop();
-		await display.calibrateBottom();
-		console.log("Calibration complete");
-	};
-
-	//calibrate();
 })();
